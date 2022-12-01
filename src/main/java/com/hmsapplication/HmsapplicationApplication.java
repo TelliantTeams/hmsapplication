7e180a35ea6ddf0bdb84f186a00f1dcdb0805708
dineshkumar <dineshkumar.varadharajan@telliant.net>
2022-12-01 18:43:40 +0530

    initial commit

---
+++ src/main/java/com/hmsapplication/HmsapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.hmsapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmsapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmsapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hmsapplication/HmsapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hmsapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmsapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
